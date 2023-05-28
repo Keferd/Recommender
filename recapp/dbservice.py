5da3f4a52bb24962a812beae0220b7a2d1672cfc
Ilnar Agzamov <agzamov001@gmail.com>
2023-05-29 02:32:05 +0500

    average and count

--- recapp/dbservice.py
+++ recapp/dbservice.py
@@ -1,6 +1,141 @@
 from sqlalchemy import true
-from wagonapp import db
+from recapp import db
 from datetime import datetime
 from flask import session, make_response, redirect, url_for, jsonify
 import bcrypt
+
+import sqlite3;
+ 
+""" ------------------------------- BOOKS ------------------------------- """
+
+# Создание таблицы книг
+# def create_books_table():
+#     rows = db.session.execute("SELECT * FROM books_csv").fetchall()
+#     try:
+#         con = sqlite3.connect("recommender_books.sqlite")
+#         cursor = con.cursor()
+
+#         query = "INSERT INTO books (id,authors, original_publication_year, original_title, title, average_rating, image_url, small_image_url) VALUES (?,?,?,?,?,?,?,?)"
+#         cursor.executemany(query, rows)
+#         con.commit()
+
+#         return {'message': "Books Table Created!"}
+#     except Exception as e:
+#         db.session.rollback()
+#         return {'message': str(e)}
+
+# Обновление количества и средних оценок книг
+def update_books_count_and_average_ratings():
+    rows_books = db.session.execute("SELECT id FROM books").fetchall()
+    try:
+        con = sqlite3.connect("recommender_books.sqlite")
+        cursor = con.cursor()
+
+        newrows = []
+
+        for book_id in rows_books:
+            rows_ratings = db.session.execute(f"SELECT rating FROM ratings WHERE book_id='{book_id[0]}'").fetchall()
+            amount = 0
+            sum = 0
+            for rating in rows_ratings:
+                amount = amount + 1
+                sum = sum + rating[0]
+            average = 0
+            if amount > 0:
+                average = round(sum/amount,2)
+            newrows.append((average, amount, book_id[0]))
+
+        query = "UPDATE books SET average_rating = ?, count_rating = ? WHERE id = ?"
+        cursor.executemany(query, newrows)
+        con.commit()
+
+        return {'message': "Books count and average ratings UPDATED!"}
+    except Exception as e:
+        db.session.rollback()
+        return {'message': str(e)}
+
+""" ------------------------------- USERS ------------------------------- """
+    
+# Создание таблицы пользователей
+# def create_users_table():
+#     try:
+#         con = sqlite3.connect("recommender_books.sqlite")
+#         cursor = con.cursor()
+
+#         rows = []
+
+#         for i in range(1,53425):
+#             rows.append((i,))
+
+#         query = "INSERT INTO users (id) VALUES (?)"
+#         cursor.executemany(query, rows)
+#         con.commit()
+
+#         return {'message': "Users Table Created!"}
+#     except Exception as e:
+#         db.session.rollback()
+#         return {'message': str(e)}
+
+# Обновление количества и средних оценок пользователей
+def update_users_count_and_average_ratings():
+    rows_users = db.session.execute("SELECT id FROM users").fetchall()
+    try:
+        con = sqlite3.connect("recommender_books.sqlite")
+        cursor = con.cursor()
+
+        newrows = []
+        
+        query = "UPDATE users SET average_rating = ?, count_rating = ? WHERE id = ?"
+
+        for user_id in rows_users:
+            rows_ratings = db.session.execute(f"SELECT rating FROM ratings WHERE user_id='{user_id[0]}'").fetchall()
+            amount = 0
+            sum = 0
+            for rating in rows_ratings:
+                amount = amount + 1
+                sum = sum + rating[0]
+            average = 0
+            if amount > 0:
+                average = round(sum/amount,2)
+            newrows.append((average, amount, user_id[0]))
+        
+        cursor.executemany(query, newrows)
+        con.commit()
+
+        return {'message': "Books count and average ratings UPDATED!"}
+    except Exception as e:
+        db.session.rollback()
+        return {'message': str(e)}
+
+""" ------------------------------- RATINGS ------------------------------- """
+
+# Создание таблицы оценок
+# def create_ratings_table():
+#     try:
+#         con = sqlite3.connect("recommender_books.sqlite")
+#         cursor = con.cursor()
+
+#         newrows = []
+
+#         for k in range(1,10001):
+#             rows = db.session.execute(f"SELECT * FROM ratings_csv WHERE book_id='{k}'").fetchall()
+#             for i in range(len(rows)): 
+#                 check = True
+#                 for j in range(0,i):
+#                     if rows[i][1] == rows[j][1]:
+#                         check = False
+#                         break
+#                 if check == True:
+#                     newrows.append(rows[i])
+#             print(k)
+        
+#         query = "INSERT INTO ratings (book_id, user_id, rating) VALUES (?,?,?)"
+#         cursor.executemany(query, newrows)
+#         con.commit()
+
+#         return {'message': "Ratings Table Created!"}
+#     except Exception as e:
+#         db.session.rollback()
+#         return {'message': str(e)}
+
 
--- recapp/routes.py
+++ recapp/routes.py
@@ -6,9 +6,49 @@ import functools
 
 import json
 
+# Главная страница
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 
+""" ------------------------------- BOOKS ------------------------------- """
+
+# Создание таблицы книг
+# @app.route('/api/create_books_table', methods=['POST'])
+# def post_books_table():
+#     response = dbservice.create_books_table()
+#     return json_response(response)
+
+# Обновление количества и средних оценок книг
+@app.route('/api/update_books_count_and_average_ratings', methods=['PUT'])
+def put_books_count_and_average_ratings():
+    response = dbservice.update_books_count_and_average_ratings()
+    return json_response(response)
+
+""" ------------------------------- USERS ------------------------------- """
+
+# Создание таблицы пользователей
+# @app.route('/api/create_users_table', methods=['POST'])
+# def post_users_table():
+#     response = dbservice.create_users_table()
+#     return json_response(response)
+
+# Обновление количества и средних оценок пользователей
+@app.route('/api/update_users_count_and_average_ratings', methods=['PUT'])
+def put_users_count_and_average_ratings():
+    response = dbservice.update_users_count_and_average_ratings()
+    return json_response(response)
+
+""" ------------------------------- RATINGS ------------------------------- """
+
+# Создание таблицы оценок
+# @app.route('/api/create_ratings_table', methods=['POST'])
+# def post_ratings_table():
+#     response = dbservice.create_ratings_table()
+#     return json_response(response)
 
 
 
