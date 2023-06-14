9bbaff09b58590b43c7b4e1a5711415aaeb48ac4
Ilnar Agzamov <agzamov001@gmail.com>
2023-06-14 05:50:42 +0500

    Интерфейс тестирования Funk SVD

--- recapp/static/javascripts/user_recommendation_funk_svd_testing.js
+++ recapp/static/javascripts/user_recommendation_funk_svd_testing.js
@@ -1,33 +1,29 @@
-let menu_user_recommendation_testing_button = document.getElementById("admin__menu_user_recommendation_testing");
+let menu_user_recommendation_funk_svd_testing = document.getElementById("admin__menu_user_recommendation_funk_svd_testing");
 
-menu_user_recommendation_testing_button.addEventListener("click", function (e) {
+menu_user_recommendation_funk_svd_testing.addEventListener("click", function (e) {
 
     let area = document.getElementById('admin__main')
     
     area.innerHTML = `
         <div class="admin__main-container">
-        <h1 class="admin__h1"> Тестирование рекомендаций по корреляции Пирсона</h1>
+        <h1 class="admin__h1"> Тестирование рекомендаций по Funk SVD</h1>
         <div class="admin__content">
         <div class="admin__form">
             <div>
-                <label for="count_rating">Минимальное количество оценок от пользователя:</label>
-                <input class="admin__input-number" type="number" min="1" value="100" id="count_rating">
+                <label for="factors">Количество факторов:</label>
+                <input class="admin__input-number" type="number" min="1" value="20" id="factors">
             </div>
             <div>
-                <label for="number_of_crossings">Минимальное количество пересечений между пользователями:</label>
-                <input class="admin__input-number" type="number" min="1" value="3" id="number_of_crossings">
+                <label for="learning_rate">Скорость обучения:</label>
+                <input class="admin__input-number" type="number" min="0" value="0.01" step="0.001" id="learning_rate">
             </div>
             <div>
-                <label for="correlation">Минимальное число корреляции:</label>
-                <input class="admin__input-number" type="number" step="0.01" max="1" min="0" value="0.3" id="correlation">
+                <label for="regularization">Регуляризация:</label>
+                <input class="admin__input-number" type="number" min="0" value="0.01" step="0.001" id="regularization">
             </div>
             <div>
-                <label for="normalization_number">Число, относительно которого происходит нормализация:</label>
-                <input class="admin__input-number" type="number" step="0.01" value="3.8" id="normalization_number">
-            </div>
-            <div>
-                <label for="percentage_tested_users">Процент пользователей для тестирования:</label>
-                <input class="admin__input-number" type="number" step="0.01" max="1" min="0" value="0.2" id="percentage_tested_users">
+                <label for="gradient_count">Количество итераций:</label>
+                <input class="admin__input-number" type="number" min="1" value="20" id="gradient_count">
             </div>
             <div>
                 <label for="percentage_tested_ratings">Процент оценок пользователей для тестирования:</label>
@@ -35,7 +31,7 @@ menu_user_recommendation_testing_button.addEventListener("click", function (e) {
             </div>
 
             <div>
-            <input class="admin__input-button" type="button" value="Запрос" id="user_recommendation_testing_button">
+            <input class="admin__input-button" type="button" value="Запрос" id="user_recommendation_funk_svd_testing">
             </div>
         </div>
         <div class="admin__result">
@@ -52,34 +48,31 @@ menu_user_recommendation_testing_button.addEventListener("click", function (e) {
 
     // -----------------------------------------------------------------------------
 
-    let user_recommendation_testing_button = document.getElementById("user_recommendation_testing_button");
+    let user_recommendation_funk_svd_testing_button = document.getElementById("user_recommendation_funk_svd_testing");
 
-    user_recommendation_testing_button.addEventListener("click", function (e) {
+    user_recommendation_funk_svd_testing_button.addEventListener("click", function (e) {
 
-        let count_rating = document.getElementById('count_rating').value;
-        let number_of_crossings = document.getElementById('number_of_crossings').value;
-        let correlation = document.getElementById('correlation').value;
-        let normalization_number = document.getElementById('normalization_number').value
-        let percentage_tested_users = document.getElementById('percentage_tested_users').value;
+        let factors = document.getElementById('factors').value;
+        let learning_rate = document.getElementById('learning_rate').value;
+        let regularization = document.getElementById('regularization').value;
+        let gradient_count = document.getElementById('gradient_count').value;
         let percentage_tested_ratings = document.getElementById('percentage_tested_ratings').value;
 
 
-        let user_formdata = JSON.stringify({count_rating: count_rating, 
-                                            number_of_crossings: number_of_crossings,
-                                            correlation: correlation,
-                                            normalization_number: normalization_number,
-                                            percentage_tested_users: percentage_tested_users,
+        let user_formdata = JSON.stringify({factors: factors,
+                                            learning_rate: learning_rate, 
+                                            regularization: regularization,
+                                            gradient_count: gradient_count,
                                             percentage_tested_ratings: percentage_tested_ratings});
 
         formparse = JSON.parse(user_formdata);
 
-        if (formparse['count_rating'] != "" && 
-            formparse['number_of_crossings'] != "" && 
-            formparse['correlation'] != "" && 
-            formparse['normalization_number'] != "" &&
-            formparse['percentage_tested_users'] != "" && 
+        if (formparse['factors'] != "" && 
+            formparse['learning_rate'] != "" && 
+            formparse['regularization'] != "" && 
+            formparse['gradient_count'] != "" && 
             formparse['percentage_tested_ratings'] != "") {
-            fetch("/api/testing_prediction_Pearson",
+            fetch("/api/testing_prediction_Funk_SVD",
             {
                 method: "POST",
                 body: user_formdata,
@@ -98,19 +91,11 @@ menu_user_recommendation_testing_button.addEventListener("click", function (e) {
                     if (request != "") {
                         area.innerHTML += `
                         <div class="admin__testing_block">
-                            <div>Количество тестируемых пользователей:</div>
-                            <div>` + request["users_count"] + `</div>
-                        </div>
-                        <div class="admin__testing_block">
-                            <div>Количество тестируемых оценок:</div>
-                            <div>` + request["rating_tasting_count"] + `</div>
-                        </div>
-                        <div class="admin__testing_block">
                             <div>Из них предсказано:</div>
                             <div>` + request["rating_recommendations_count"] + `</div>
                         </div>
                         <div class="admin__testing_block">
-                            <div>Метрика MAE по корреляции Пирсона:</div>
+                            <div>Метрика MAE по Funk SVD:</div>
                             <div>` + request["mae_metrics"] + `</div>
                         </div>
                         <div class="admin__testing_block">
@@ -118,7 +103,7 @@ menu_user_recommendation_testing_button.addEventListener("click", function (e) {
                             <div>` + request["mae_metrics_compared"] + `</div>
                         </div>
                         <div class="admin__testing_block">
-                            <div>Метрика MSE по корреляции Пирсона:</div>
+                            <div>Метрика MSE по Funk SVD:</div>
                             <div>` + request["mse_metrics"] + `</div>
                         </div>
                         <div class="admin__testing_block">
@@ -126,7 +111,7 @@ menu_user_recommendation_testing_button.addEventListener("click", function (e) {
                             <div>` + request["mse_metrics_compared"] + `</div>
                         </div>
                         <div class="admin__testing_block">
-                            <div>Метрика RMSE по корреляции Пирсона:</div>
+                            <div>Метрика RMSE по Funk SVD:</div>
                             <div>` + request["rmse_metrics"] + `</div>
                         </div>
                         <div class="admin__testing_block">
